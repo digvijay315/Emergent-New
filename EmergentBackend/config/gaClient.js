b51c7035c60e42c82d85efb5c6b4912cea99dce9
digvijay315 <143862901+digvijay315@users.noreply.github.com>
2026-01-15 23:10:32 +0530

    update

--- EmergentBackend/config/gaClient.js
+++ EmergentBackend/config/gaClient.js
@@ -1,7 +1,28 @@
-const { BetaAnalyticsDataClient } = require("@google-analytics/data");
+// const { BetaAnalyticsDataClient } = require("@google-analytics/data");
 
+// const analyticsClient = new BetaAnalyticsDataClient({
+//   keyFilename: "ga-service-account.json",
+// });
+
+// module.exports = analyticsClient;
+
+const { BetaAnalyticsDataClient } = require("@google-analytics/data");
+const path = require("path");
+const fs = require("fs");
+
+// Absolute path to your JSON
+const keyPath = path.join(__dirname, "ga-service-account.json");
+
+// Read JSON
+const keyFile = JSON.parse(fs.readFileSync(keyPath, "utf8"));
+
+// Fix line breaks (critical for Linux/AWS)
+keyFile.private_key = keyFile.private_key.replace(/\\n/g, "\n");
+
+// Create GA client
 const analyticsClient = new BetaAnalyticsDataClient({
-  keyFilename: "ga-service-account.json",
+  credentials: keyFile,
 });
 
 module.exports = analyticsClient;
+

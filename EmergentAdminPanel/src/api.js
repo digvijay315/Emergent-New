31582dec03b1ca47740acff5cd449f403859162d
digvijay315 <143862901+digvijay315@users.noreply.github.com>
2026-01-19 13:20:20 +0530

    update

--- EmergentAdminPanel/src/api.js
+++ EmergentAdminPanel/src/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 const instance=axios.create({
    
         //   baseURL:'http://localhost:5000/'
-        //   baseURL:'http://localhost:5000/'
           baseURL:"https://api.intelliviaai.com/"
 
        
